6b0d6d458454f0cea93dc6e6e618f0ca821af12e
Lia <lia.erisson@mail.mcgill.ca>
2026-08-11 14:05:21 -0400

    Refactor Order class: enforce positive price and quantity validation, update member variables to const

--- simulator.cpp
+++ simulator.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-#include <string>
-#include <cstring>
 #include <cstdint>
 #include <stdexcept>
 
@@ -11,28 +8,34 @@ enum class Side {
 
 class Order {
     private:
-        uint64_t orderId;
-        uint64_t traderId;
-        int64_t price;
-        Side side;
-        //unsigned int with validation
-        uint64_t quantity;
-        int sequenceNumber;
+        const std::uint64_t orderId;
+        const std::uint64_t traderId;
+        const std::int64_t price;
+        const Side side;
+        std::uint64_t quantity;
+        const int sequenceNumber;
 
-    public:
-        Order(uint64_t orderId, uint64_t traderId, Side side, int64_t price, uint64_t quantity, int sequenceNumber) {
+        static std::int64_t validatePrice(std::int64_t price) {
             if(price <= 0) {
                 throw std::invalid_argument("Price must be positive.");
-            } else if(quantity == 0) {
+            }
+            return price;
+        }
+
+        static std::uint64_t validateQuantity(std::uint64_t quantity) {
+            if(quantity == 0) {
                 throw std::invalid_argument("Quantity must be >0.");
             }
+            return quantity;
+        }
 
-            this->orderId = orderId;
-            this->traderId = traderId;
-            this->price = price;
-            this->side = side;
-            this->quantity = quantity;
-            this->sequenceNumber = sequenceNumber;
+    public:
+        Order(std::uint64_t orderId, std::uint64_t traderId, 
+            Side side, std::int64_t price, 
+            std::uint64_t quantity, int sequenceNumber) 
+            : orderId(orderId), traderId(traderId), side(side), sequenceNumber(sequenceNumber), price(validatePrice(price)), quantity(validateQuantity(quantity))
+            {
+            
         }
 
         void reduceQuantity(std::uint64_t purchased) {
