28023798863e058c6a01f74a4b3d6c4694c791bd
Lia <lia.erisson@mail.mcgill.ca>
2026-08-13 10:38:16 -0400

    feat: enhance order cancellation tests in main program and update notes

--- main.cpp
+++ main.cpp
@@ -8,16 +8,61 @@
 #include <unordered_map>
 #include "OrderBook.h"
 
+//Mock Unit Testing
 int main() {
     OrderBook book;
 
+    //correct order addition
     book.addOrder(10, 10000, Side::BUY, 50);
-    book.addOrder(20, 10000, Side::BUY, 30);
+    if (book.containsOrder(1)) {
+        std::cout << "Order successfully added.\n";
+    }
 
+    book.addOrder(20, 10000, Side::BUY, 30);
+    
+    //correct order cancellation on buy side
     book.cancelOrder(1, 10);
 
     if (!book.containsOrder(1)) {
         std::cout << "Order successfully cancelled.\n";
+    }
+
+    //correct order cancellation on sell side
+    book.addOrder(22, 10000, Side::SELL, 30);
+    if (book.containsOrder(3)) {
+        std::cout << "Order successfully added.\n";
+    }
+
+    book.cancelOrder(3, 22);
+
+    if (!book.containsOrder(3)) {
+        std::cout << "Order successfully cancelled.\n";
+    }
+
+    //checks full correct removal of order
+    try {
+        book.cancelOrder(3, 22);
+    } catch (const std::invalid_argument& e) {
+        std::cout << e.what() << '\n';
+    }
+
+    //attempted cancellation by wrong trader
+    book.addOrder(2, 10000, Side::SELL, 30);
+    try {
+        book.cancelOrder(4, 20);
+    } catch (const std::invalid_argument& e) {
+        std::cout << e.what() << '\n';
+    }
+
+    if (book.containsOrder(4)) {
+        std::cout << "Order was correctly preserved.\n";
+    }
+
+    //catches nonexistent orders
+    try {
+        book.cancelOrder(9, 2);
+    } catch (const std::invalid_argument& e) {
+        std::cout << e.what() << '\n';
     }
 
     return 0;
