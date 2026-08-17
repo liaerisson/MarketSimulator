2fe76c1e1dfbe526c73b39d1175295b1966a96db
Lia <lia.erisson@mail.mcgill.ca>
2026-08-17 08:51:16 -0400

    test: add additional test cases for Order class functionality

--- tests/OrderTests.cpp
+++ tests/OrderTests.cpp
@@ -21,3 +21,38 @@ TEST_CASE("Order not thrown if valid") {
         Order(1, 10, Side::BUY, 10000, 10, 1)
     );
 }
+
+TEST_CASE("Order values stored correctly") {
+    Order newOrder = Order(1, 10, Side::BUY, 10000, 10, 1);
+    REQUIRE(newOrder.getOrderId() == 1);
+    REQUIRE(newOrder.getTraderId() == 10);
+    REQUIRE(newOrder.getSide() == Side::BUY);
+    REQUIRE(newOrder.getPrice() == 10000); 
+    REQUIRE(newOrder.getQuantity() == 10);
+}
+
+TEST_CASE("Order rejects reduction of 0") {
+    Order newOrder = Order(1, 10, Side::BUY, 10000, 10, 1);
+    REQUIRE_THROWS_AS(
+        newOrder.reduceQuantity(0), std::invalid_argument
+    );
+}
+
+TEST_CASE("Order rejects reduction greater than quantity") {
+    Order newOrder = Order(1, 10, Side::BUY, 10000, 10, 1);
+    REQUIRE_THROWS_AS(
+        newOrder.reduceQuantity(11), std::invalid_argument
+    );
+}
+
+TEST_CASE("Order reduces from quantity correctly") {
+    Order newOrder = Order(1, 10, Side::BUY, 10000, 10, 1);
+    newOrder.reduceQuantity(5);
+    REQUIRE(newOrder.getQuantity() == 5);
+}
+
+TEST_CASE("Order reduces full quantity correctly") {
+    Order newOrder = Order(1, 10, Side::BUY, 10000, 10, 1);
+    newOrder.reduceQuantity(10);
+    REQUIRE(newOrder.getQuantity() == 0);
+}
